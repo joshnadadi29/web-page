d34ec05d6cb2ee38a48a7e35a66083bb9f1a0dac
joshnadadi29 <joshnadadi8@gmail.com>
2025-08-08 19:58:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const options = [
-  "Order Pizza ",
-  "Watch a Comedy ",
-  "Take a Walk ",
-  "Call a Friend ",
-  "Read a Book ",
-  "Drink Water ",
-  "Dance for 1 Minute "
+  "Order Pizza",
+  "Watch a Comedy",
+  "Take a Walk",
+  "Call a Friend",
+  "Read a Book",
+  "Drink Water",
+  "Dance for 1 Minute"
 ];
 
 const spinBtn = document.getElementById("spinBtn");
@@ -14,6 +14,5 @@ const resultDiv = document.getElementById("result");
 spinBtn.addEventListener("click", () => {
   const randomIndex = Math.floor(Math.random() * options.length);
   const decision = options[randomIndex];
-  resultDiv.textContent = 🎉 ${decision};
+  resultDiv.textContent = `🎉 ${decision}`;
 });
-
